3b12a0d646c34fbdf028b49f33415c1ed3149b85
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-07-10 18:06:58 +0300

    Implements fetching request from API

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+   .then(function (res){
+    return res.json();
+   })
+   .then (function (data){
+    return renderBooks(data)
+   })
+
 }
 
 function renderBooks(books) {
